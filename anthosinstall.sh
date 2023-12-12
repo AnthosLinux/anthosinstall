07e1cfb019339403e2dab7d06e21fe9486a03653
Richard Benson <91757099+rendick@users.noreply.github.com>
2023-12-13 01:34:59 +0200

    function locale{} 50%

--- anthosinstall.sh
+++ anthosinstall.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-while true; do
-    echo "Hello World"
-    sleep 5
-done
+red="\e[31m"
+none="\e[0m"
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+function locale() {
+    echo ""
+	ls /usr/share/kbd/keymaps/**/*.map.gz
+    echo ""
+    read -e -p "${bold}Do you want to change the keyboard locale? (y or n)${normal}" choice
+}
+
+locale
+
+while [ "$choice" != "y" ]; do
+    if [ "$choice" = "n" ]; then
+        echo "you select no"
+        sleep 1
+        locale
+    fi
+done
