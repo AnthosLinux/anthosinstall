01b03128dc690043f27d7aa073d654c7718d835c
richard <kostianaddur@gmail.com>
2023-12-16 13:55:44 +0200

    function checking(){ ... }

--- anthosinstall.sh
+++ anthosinstall.sh
@@ -7,15 +7,26 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 function checking() {
-    if [ "$(cat /sys/firmware/efi/fw_platform_size)" -eq 64 ]; then
+    # Flatform size 
+	if [ "$(cat /sys/firmware/efi/fw_platform_size)" -eq 64 ]; then
         echo -e "${bold}You are using 64-bit platform!${normal}"
-        echo "Continue..."
     else
         echo "You are not using 64-bit platform!"
         exit
     fi
+	
+	# Locale
+    echo "The default locale is English."
 
-    echo "The default locale is English..."
+	# Check internet connection
+	wget -q --spider https://duckduckgo.com/
+
+	if [ $? -eq 0 ]; then
+		echo "You are connected to the internet!"
+	else
+		echo -e "${bold}${red}You are not connected to the internet!${red}${normal}"
+		exit
+	fi
 
     for i in {1..5}; do
         echo -n "$i.."
@@ -26,15 +37,10 @@ function checking() {
 
 checking
 
-sleep 2
-
-# function internet(){
-# 	
-#
-# }
-#
-# internet
+sleep 1
 
 function pacman(){
 	pacman-key --init
 }
+
+
